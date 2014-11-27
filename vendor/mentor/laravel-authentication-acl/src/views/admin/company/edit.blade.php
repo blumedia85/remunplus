ca3f3a9dcb0cd612f3f80a1ae01932b01dfe0ebc
Zulqarnain Abbas <zabbas44@gmail.com>
2014-11-27 20:26:09 +0500

    client form

--- vendor/mentor/laravel-authentication-acl/src/views/admin/company/edit.blade.php
+++ vendor/mentor/laravel-authentication-acl/src/views/admin/company/edit.blade.php
@@ -25,13 +25,13 @@ Admin area: edit client
                 </div>
             </div>
             <div class="panel-body">
-                <div class="row">
+<!--                <div class="row">
                     <div class="col-md-12 col-xs-12">
                         <a href="{{URL::action('Mentordeveloper\Authentication\Controllers\ClientController@postEditProfile',["user_id" => $user->id])}}" class="btn btn-info pull-right" {{! isset($user->id) ? 'disabled="disabled"' : ''}}><i class="fa fa-user"></i> Edit profile</a>
                     </div>
-                </div>
+                </div>-->
                 <div class="col-md-12 col-xs-12">
-                    <h4>Login data</h4>
+                    <h4>Client data</h4>
                     {{Form::model($user, [ 'url' => URL::action('Mentordeveloper\Authentication\Controllers\ClientController@postEditClient')] ) }}
                     {{-- Field hidden to fix chrome and safari autocomplete bug --}}
                     {{Form::password('__to_hide_password_autocomplete', ['class' => 'hidden'])}}
@@ -54,6 +54,46 @@ Admin area: edit client
                         {{Form::password('password_confirmation', ['class' => 'form-control', 'placeholder' => '','autocomplete' => 'off'])}}
                     </div>
                     <span class="text-danger">{{$errors->first('password_confirmation')}}</span>
+                    
+                    <!-- Compnay Name text field -->
+                    <div class="form-group">
+                        {{Form::label('c_name','Company Name: *')}}
+                        {{Form::text('c_name', null, ['class' => 'form-control', 'placeholder' => 'Company name', 'autocomplete' => 'off'])}}
+                    </div>
+                    <span class="text-danger">{{$errors->first('c_name')}}</span>
+                    
+                    <!-- Employer Name text field -->
+                    <div class="form-group">
+                        {{Form::label('emp_name','Employer Name: *')}}
+                        {{Form::text('emp_name', null, ['class' => 'form-control', 'placeholder' => 'Employer Name', 'autocomplete' => 'off'])}}
+                    </div>
+                    <span class="text-danger">{{$errors->first('emp_name')}}</span>
+                    
+                    <!-- Employer Number text field -->
+                    <div class="form-group">
+                        {{Form::label('emp_number','Employer Number: *')}}
+                        {{Form::text('emp_number', null, ['class' => 'form-control', 'placeholder' => 'Employer Number', 'autocomplete' => 'off'])}}
+                    </div>
+                    <span class="text-danger">{{$errors->first('emp_number')}}</span>
+                    
+                    <!-- Address text field -->
+                    <div class="form-group">
+                        {{Form::label('address','Address: *')}}
+                        {{Form::textarea ('address', null, ['class' => 'form-control', 'placeholder' => 'user email', 'autocomplete' => 'off'])}}
+                    </div>
+                    <span class="text-danger">{{$errors->first('address')}}</span>
+                   
+                    <div class="form-group">
+                        {{Form::label("parish","Parish: ")}}
+                        {{Form::select('parish', ["1" => "parish 1", "0" => "parish 2"], (isset($user->activated) && $user->activated) ? $user->activated : "0", ["class"=> "form-control"] )}}
+                    </div>
+                    <!-- Contact # text field -->
+                    <div class="form-group">
+                        {{Form::label('contact_number','Address: *')}}
+                        {{Form::text ('contact_number', null, ['class' => 'form-control', 'placeholder' => 'Contact #', 'autocomplete' => 'off'])}}
+                    </div>
+                    <span class="text-danger">{{$errors->first('contact_number')}}</span>
+                    
                     <div class="form-group">
                         {{Form::label("activated","Client active: ")}}
                         {{Form::select('activated', ["1" => "Yes", "0" => "No"], (isset($user->activated) && $user->activated) ? $user->activated : "0", ["class"=> "form-control"] )}}
